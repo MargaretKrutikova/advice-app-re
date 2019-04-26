2f5f71be2f8660c1fc094fb451c5864574f26373
Margarita Krutikova <margaretkru@gmail.com>
2019-04-27 01:21:08 +0200

    Initial commit, styling

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+const outputDir = path.join(__dirname, "build/")
+
+const isProd = process.env.NODE_ENV === "production"
+
+module.exports = {
+  entry: "./src/Index.bs.js",
+  mode: isProd ? "production" : "development",
+  output: {
+    path: outputDir,
+    filename: "Index.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      inject: false
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  devServer: {
+    compress: true,
+    contentBase: outputDir,
+    port: process.env.PORT || 8000,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+      }
+    ]
+  }
+}
